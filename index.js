d614d5726a69091a2e5198dcbdb1927992172f2f
Avinash Sarguru <avinashsarguru@gmail.com>
2019-07-31 23:20:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(muscians, instruments){
   var ar = [];
   for (var i = 0; i < muscians.length; i++){
-    ar[i] = muscians[i] + instruments[i];
+    ar[i] = muscians[i] + " plays " + instruments[i];
   }
   return ar;
   }
