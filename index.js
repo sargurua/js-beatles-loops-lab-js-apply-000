203c24edca6e902f449c2366c0aa3214455e4dc3
Avinash Sarguru <avinashsarguru@gmail.com>
2019-07-31 23:24:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // add solution here
 function theBeatlesPlay(muscians, instruments){
   var ar = [];
-  for (var i = 0; i < muscians.length; i++){
+  for (var i = 0; i < muscians.length; i++)
     ar[i] = muscians[i] + " plays " + instruments[i];
-  }
   return ar;
-  }
+}
+
+function johnLennonFacts(facts){
+  for (var i = 0; i < facts.length; i++)
+    facts[i] += "!!!";
+  return facts;
+}
+  
